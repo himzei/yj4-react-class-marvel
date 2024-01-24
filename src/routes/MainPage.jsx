9f5dd9e1e38cba2f6fcb793c9dd6d1cfd1136202
himzei <himzei@gmail.com>
2024-01-24 11:51:33 +0900

    오류

--- src/routes/MainPage.jsx
+++ src/routes/MainPage.jsx
@@ -4,6 +4,9 @@ import Layout from "../components/Layout";
 import NoticeDisney from "../components/NoticeDisney";
 import { testimonials } from "../lib/menus";
 import { motion } from "framer-motion"
+import Facebook from "../assets/Facebook";
+import Insta from "../assets/Insta";
+import Pinterest from "../assets/Pinterest";
 
 const Card = ({ 
     image, 
@@ -19,19 +22,19 @@ const Card = ({
         <div className="relative w-full h-full flex justify-center">
             <motion.div 
                 initial={false}
+                style={{
+                    zIndex: position
+                }}
                 animate={{
-                    x: `${offset}`
+                    x: `${offset}%`
                 }}
                 transition={{
                     duration: 0.25, 
                     ease: "easeOut"
                 }}
-                style={{
-                    zIndex: position
-                }}
                 onClick={() => setSelected(position)}
                 className="absolute top-0 left-0 w-full h-full flex justify-center">
-                <img className="w-full h-full object-cover" src={image} alt={title} />
+                    <img className="w-full h-full object-cover" src={image} alt={title} />
             </motion.div>
             <div className="absolute max-w-7xl w-full h-full flex flex-col text-white space-y-4 justify-center">
                 <h1 className="text-4xl font-bold uppercase">echo comics</h1>
@@ -42,18 +45,58 @@ const Card = ({
     )
 }
 
-const SelectedBtns = ({numTracks}) => {
+const SelectedBtns = ({numTracks, setSelected, selected}) => {
     return (
-        <>  
-            {numTracks.map((item, index) => (
-                <div key={index}>버튼</div>
+        <div className="flex space-x-2">  
+            {numTracks.map((item, index, array) => (
+                <button 
+                    onClick={() => setSelected(index)} 
+                    key={index}
+                    className="h-2 w-full bg-slate-300 relative"
+                >
+                    {
+                        selected === index ? (
+
+                            <motion.span 
+                                className="absolute top-0 left-0 bg-red-600 w-full h-full"
+                                initial={{
+                                    width: "0%"
+                                }}
+                                animate={{
+                                    width: "100%"
+                                }}
+                                transition={{
+                                    duration: 2, 
+                                    ease: "easeOut"
+                                }}
+                                onAnimationComplete={() => {
+                                    setSelected(selected === array.length - 1 ? 0 : selected + 1)
+                                }}
+                               >
+                                    
+
+                            </motion.span>
+                        ):(
+                            <span className="absolute top-0 left-0 bg-red-600" style={{
+                                width: selected > index ? "100%" : "0%"
+                            }}>
+
+                            </span>
+                        )
+                    }
+                   <p 
+                   className={`w-full h-16 text-left items-start pt-4 px-1 text-gray-500 ${selected === index && "text-red-600"} uppercase`}> 
+                    {item.title}
+                   </p>
+                </button>
             ))}
-        </>
+        </div>
     )
 }
 
 export default function MainPage(){
-    const [selected, setSelected ] = useState(2);
+    const [selected, setSelected ] = useState(0);
+    console.log("selected", selected)
     return (
         <>
             <Layout>
@@ -80,11 +123,19 @@ export default function MainPage(){
                     <div className="w-full h-20 flex justify-center">
                         <div className="max-w-7xl h-full w-full grid grid-cols-4">
                             {/* 1: 75% grid-cols-3 */}
-                            <div className="col-span-3 bg-white">
-                                <SelectedBtns numTracks={testimonials} />
+                            <div className="col-span-3 -translate-y-12 bg-white">
+                                <SelectedBtns 
+                                    numTracks={testimonials}
+                                    setSelected={setSelected}
+                                    selected={selected}
+                                />
                             </div>
                             {/* 2: 25% grid-cols-1 */}
-                            <div className="bg-blue-500"></div>
+                            <div className="flex w-full h-full justify-end space-x-4 items-center">
+                                <Facebook />
+                                <Insta />
+                                <Pinterest />
+                            </div>
                         </div>
                     </div>
                 </section>
