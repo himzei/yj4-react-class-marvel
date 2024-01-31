07532c082ac96856a38f9cadeefe732ee8b30bec
himzei <himzei@gmail.com>
2024-01-31 09:17:24 +0900

    0131

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
-import MainPage from './routes/MainPage.jsx'
-import NotFound from './routes/NotFound.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import MainPage from "./routes/MainPage.jsx";
+import NotFound from "./routes/NotFound.jsx";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 const router = createBrowserRouter([
   {
-    path: "/", 
+    path: "/",
     element: <Outlet />,
     errorElement: <NotFound />,
     children: [
       {
-        path: "", 
-        element: <MainPage />
-      }
-    ]
-  }
-])
+        path: "",
+        element: <MainPage />,
+      },
+    ],
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
